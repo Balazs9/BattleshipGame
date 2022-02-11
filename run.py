553f0a7619232b09cd726297dcd27d8a0531abc5
Balazs9 <sempergerbalazs@gmail.com>
2022-02-11 17:00:17 +0000

    add style and game rules

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import random
 from random import randint
 from termcolor import colored
 
-
 ship_in_row = random.randint(0, 9)
 ship_in_col = random.randint(0, 9)
 
@@ -20,8 +19,9 @@ class Game:
         self.player_name = player_name
         self.game_turn = game_turn
         self.player = []
-        self.computer = []
-               
+        self.ship_in_row = random.randint(0, 9)
+        self.ship_in_col = random.randint(0, 9)
+
     def print_board(self):
         """
         Print the game area
@@ -33,10 +33,11 @@ class Game:
         """
         Player guess the ship coordinates
         """
-        for game_turn in range(5):
+        for game_turn in range(7):
             print("Game", game_turn + 1)
             player_guess_row = int(input("number: \n"))
             player_guess_col = int(input("number: \n"))
+
             if(player_guess_row == ship_in_row and player_guess_col == ship_in_col):
                 self.player.append((player_guess_row, player_guess_col))
                 self.board[player_guess_row][player_guess_col] = "*"
@@ -72,12 +73,26 @@ class Game:
         """
         Game turn, 5 chance for each player to guess
         """
-
         for game in game_turn:
-            if(game == 5):
+            if game == 5:
                 print("Game over")
             else:
                 game += 1
+
+    def validate_guess(self, player_guess, computer_guess):
+        """
+        Validate given guesses, player give the value,
+        validator checks if is an integer, if not
+        ValueError
+        """
+        try:
+            for value in player_guess, computer_guess:
+                if 0 > value > 9:
+                    raise ValueError(
+                        "Sorry must be a number between 0 and 9!")
+        except ValueError as e:
+            print(f"Sorry {e} is not a number, you must enter a number")
+            return False
 
 
 def new_game():
@@ -85,9 +100,27 @@ def new_game():
     new game is calling the game functions
     """
     size = 9
-    game_turn = 5
+    game_turn = 7
     number_of_ships = 5
-    player_name = input("Please enter your name:")
+    print(colored("      * ", "yellow"))
+    print(colored("      ***** ", "yellow"))
+    print(colored("      *   *  ", "yellow"))
+    print(colored("      *   *  ", "yellow"))
+    print(colored("*     *   *    *", "yellow"))
+    print(colored(" **************", "yellow"))
+    print(colored("  ************", "yellow"))
+    print(colored("   **********", "yellow"))
+    print(colored("*" * 35, "blue"))
+    print(colored("*" * 35, "blue"))
+    print(colored("*" * 35, "blue"))
+    print(colored("*" * 35, "red"))
+    print(colored("Game Rules: ", "yellow"))
+    print(colored("9x9 matrix", "yellow"))
+    print(colored("Player against the computer", "yellow"))
+    print(colored("Each the player and the computer has 5 chance to guess the place of the ship", "yellow"))
+    print(colored("validator checking if the given guesses are correct"))
+    player_name = input(colored("Please enter your name: ", "red"))
+    print(colored(f"Hi {player_name}, enjoy the fight", "yellow"))
     game = Game(size, number_of_ships, player_name, game_turn)
     game.print_board()
     game.player_guess()
